2419ede649469d01def818cee25ae8cc4616fb2b
Daniel McDonald <mcdonadt@colorado.edu>
2014-03-19 10:55:33 -0600

    pep8ing, explicitly ignoring an E501

--- pyqi/commands/make_command.py
+++ pyqi/commands/make_command.py
@@ -11,11 +11,10 @@
 __credits__ = ["Daniel McDonald", "Greg Caporaso", "Doug Wendel",
                "Jai Ram Rideout"]
 
-from pyqi.core.command import (Command, CommandIn, CommandOut, 
-    ParameterCollection)
+from pyqi.core.command import CommandIn, CommandOut, ParameterCollection
 from pyqi.commands.code_header_generator import CodeHeaderGenerator
 
-command_imports = """from pyqi.core.command import (Command, CommandIn, CommandOut, 
+command_imports = """from pyqi.core.command import (Command, CommandIn, CommandOut,
     ParameterCollection)"""
 
 command_format = """class %s(Command):
@@ -56,43 +55,44 @@ class %(name)sTests(TestCase):
 if __name__ == '__main__':
     main()"""
 
+
 class MakeCommand(CodeHeaderGenerator):
     BriefDescription = "Construct a stubbed out Command object"
     LongDescription = ("This command is intended to construct the basics of a "
-        "Command object so that a developer can dive straight into the "
-        "implementation of the command")
+                       "Command object so that a developer can dive straight "
+                       "into the implementation of the command")
 
     CommandIns = ParameterCollection(
-          CodeHeaderGenerator.CommandIns.Parameters + [
-          CommandIn(Name='name', DataType=str,
-                    Description='the name of the Command', Required=True),
-          CommandIn(Name='test_code', DataType=bool,
-                    Description='create stubbed out unit test code',
-                    Required=False, Default=False)
-          ]
+        CodeHeaderGenerator.CommandIns.Parameters + [
+            CommandIn(Name='name', DataType=str,
+                      Description='the name of the Command', Required=True),
+            CommandIn(Name='test_code', DataType=bool,
+                      Description='create stubbed out unit test code',
+                      Required=False, Default=False)
+        ]
     )
     CommandOuts = ParameterCollection([
-          CommandOut(Name='result',DataType=list, 
-                    Description='The resulting template')
-          ]
+        CommandOut(Name='result', DataType=list,
+                   Description='The resulting template')
+    ]
     )
 
     def run(self, **kwargs):
         code_header_lines = super(MakeCommand, self).run(
-                author=kwargs['author'], email=kwargs['email'],
-                license=kwargs['license'], copyright=kwargs['copyright'],
-                version=kwargs['version'], credits=kwargs['credits'])['result']
+            author=kwargs['author'], email=kwargs['email'],
+            license=kwargs['license'], copyright=kwargs['copyright'],
+            version=kwargs['version'], credits=kwargs['credits'])['result']
 
         result_lines = code_header_lines
 
         if kwargs['test_code']:
             result_lines.extend(
-                    (test_format % {'name': kwargs['name']}).split('\n'))
+                (test_format % {'name': kwargs['name']}).split('\n'))
         else:
             result_lines.extend(command_imports.split('\n'))
             result_lines.append('')
             result_lines.extend((command_format % (
-                    kwargs['name'], kwargs['name'])).split('\n'))
+                kwargs['name'], kwargs['name'])).split('\n'))
 
         return {'result':  result_lines}
 
