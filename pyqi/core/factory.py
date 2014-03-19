f59fbf0fdd46867396a62940cd5fb5f2fcba69d9
Daniel McDonald <mcdonadt@colorado.edu>
2014-03-19 11:09:22 -0600

    peping

--- pyqi/core/factory.py
+++ pyqi/core/factory.py
@@ -11,18 +11,24 @@
 __credits__ = ["Greg Caporaso", "Daniel McDonald", "Doug Wendel",
                "Jai Ram Rideout"]
 
+
 def general_factory(command_constructor, usage_examples, inputs, outputs,
                     version, interface=None):
     """Generalized interface factory"""
     class IObject(interface):
+
         """Dynamic interface object"""
         CommandConstructor = command_constructor
+
         def _get_usage_examples(self):
             return usage_examples
+
         def _get_inputs(self):
             return inputs
+
         def _get_outputs(self):
             return outputs
+
         def _get_version(self):
             return version
 
